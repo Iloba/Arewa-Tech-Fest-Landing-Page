8f668a126d821737cac5ba226152ec9403b536a2
Iloba <ilobatimothy@gmail.com>
2024-06-26 09:21:09 +0100

    changes

--- js/index.js
+++ js/index.js
@@ -1,33 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('.nav-link');
+  const navLinks = document.querySelectorAll('.nav-link');
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            // Remove 'active' class from all links
-            navLinks.forEach(nav => nav.classList.remove('active'));
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      // Remove 'active' class from all links
+      navLinks.forEach(nav => nav.classList.remove('active'));
 
-            // Add 'active' class to the clicked link
-            link.classList.add('active');
-        });
+      // Add 'active' class to the clicked link
+      link.classList.add('active');
     });
+  });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    showSection('masterclass', document.getElementById('masterclassBtn'));
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  showSection('masterclass', document.getElementById('masterclassBtn'));
+});
 
 function showSection(sectionId, button) {
-    // Hide all sections
-    document.querySelectorAll('.section').forEach(section => {
-      section.style.display = 'none';
-    });
+  // Hide all sections
+  document.querySelectorAll('.section').forEach(section => {
+    section.style.display = 'none';
+  });
 
-    document.querySelectorAll('.menu-box button').forEach(btn => {
-      btn.classList.remove('active-event');
-    });
-  
-    // Display the selected section
-    document.getElementById(sectionId).style.display = 'block';
+  document.querySelectorAll('.menu-box button').forEach(btn => {
+    btn.classList.remove('active-event');
+  });
 
-    button.classList.add('active-event');
-  }
+  // Display the selected section
+  document.getElementById(sectionId).style.display = 'block';
+
+  button.classList.add('active-event');
+}
+
+//countdown
